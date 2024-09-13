91e5f0044642273520f159ff13c5fe586fc779de
travel <levqrt@gmail.com>
2024-09-13 19:54:55 +0300

    Add files via upload

---
+++ src/setting.ts
@@ -0,0 +1,27 @@
+export const Choice = {
+    style:1,
+};
+
+export const ModuleActivated = {
+    okx : true,
+    bridge : true,
+    echoDex : true,
+    horizonDex : true,
+    domen : true
+};
+
+export const Okx = {
+    
+}
+
+export const Bridge = {
+    amount: '0.005'
+};
+
+export const Dex = {
+    procent : 50,
+    slippage : 0.5
+};
+
+
+
